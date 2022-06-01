94e17cc8536f5378e0ac3b15482790cd221eb431
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-01 11:36:27 +0300

    Ideti pavyzdziai kaip nuskaityti objektus is tekstinio failo

---
+++ ObjektoNuskaitymasIsFailo/Knyga.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektoNuskaitymasIsFailo
+{
+    internal class Knyga
+    {
+        // Kintamieji
+        private string isbn;
+        private string pavadinimas;
+        private string autorius;
+        private int leidimoMetai;
+        private string leidejas;
+        private string salis;
+        private decimal kaina;
+
+        // Konstruktoriai
+        public Knyga(string isbn, string pavadinimas, string autorius, int leidimoMetai, string leidejas, string salis, decimal kaina)
+        {
+            this.isbn = isbn;
+            this.pavadinimas = pavadinimas;
+            this.autorius = autorius;
+            this.leidimoMetai = leidimoMetai;
+            this.leidejas = leidejas;
+            this.salis = salis;
+            this.kaina = kaina;
+        }
+        public Knyga()
+        {
+        }
+
+        // Getteriai setteriai
+        public string Isbn
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+        public string Pavadinimas
+        {
+            get { return pavadinimas; }
+            set { pavadinimas = value; }
+        }
+        public string Autorius
+        {
+            get { return autorius; }
+            set { autorius = value; }
+        }
+        public int LeidimoMetai
+        {
+            get { return leidimoMetai; }
+            set { leidimoMetai = value; }
+        }
+        public string Leidejas
+        {
+            get { return leidejas; }
+            set { leidejas = value; }
+        }
+        public string Salis
+        {
+            get { return salis; }
+            set { salis = value; }
+        }
+        public decimal Kaina
+        {
+            get { return kaina; }
+            set { kaina = value; }
+        }
+
+    }
+}
--- ObjektoNuskaitymasIsFailo/Program.cs
+++ ObjektoNuskaitymasIsFailo/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ObjektoNuskaitymasIsFailo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Knyga> knygos = NuskaitytiKnygasIsFailo();
+
+            foreach (Knyga knyga in knygos)
+            {
+                Console.WriteLine(knyga.Pavadinimas);
+            }
+
+        }
+
+        /// <summary>
+        ///  Funkcija nuskaito Knyga klasės objektus iš tekstinio failo ir juos išsaugo į Listą.
+        /// </summary>
+        /// <returns>Grąžina Knyga objektų Listą</returns>
+        static List<Knyga> NuskaitytiKnygasIsFailo()
+        {
+            List<Knyga> knygaList = new List<Knyga>();
+
+            // Nuskaitau kiekvieną tekstinę eilutę iš failo - į string kintamuosius, o jie yra string masyve (tai kiekvienas masyvo elementas yra atskira eilutė)
+            string[] eilutes = System.IO.File.ReadAllLines(@"C:\Users\ADFADFDA\Desktop\pamokos6-NET-programavimas\c-sharp-pamokos\pamoka11.objektu-nuskaitymas-is-failo\FailuNuskaitymas\ObjektoNuskaitymasIsFailo\knygu_duomenys.csv");
+            
+            for (int i = 1; i < eilutes.Length; i++)
+            {
+                Knyga knyga = KonvertuotiEiluteIKnyga(eilutes[i]);
+                knygaList.Add(knyga);
+            }
+            return knygaList;
+        }
+
+
+        static Knyga KonvertuotiEiluteIKnyga(string eilute)
+        {
+            Knyga knyga = new Knyga();
+
+            string[] stulpeliai = eilute.Split(',');
+
+            // Sudedu visus stulpelius į atitinkamus Knygos kintamuosoius
+            knyga.Isbn = stulpeliai[0];
+            knyga.Pavadinimas = stulpeliai[1];
+            knyga.Autorius = stulpeliai[2];
+            knyga.LeidimoMetai = Convert.ToInt32(stulpeliai[3]);
+            knyga.Leidejas = stulpeliai[4];
+            knyga.Salis = stulpeliai[5];
+            knyga.Kaina = Convert.ToDecimal(stulpeliai[6]);
+            
+
+            return knyga;
+
+        }
+    }
+}
